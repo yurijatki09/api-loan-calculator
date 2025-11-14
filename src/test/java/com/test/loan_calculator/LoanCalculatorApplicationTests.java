c91cdb73b9d544b7a8f16bfcc6441c4f8430c64a
yurijatki <82046569+yurijatki@users.noreply.github.com>
2025-11-13 21:55:59 -0300

    Commit Inicial - Loan Calculator API

---
+++ src/test/java/com/test/loan_calculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.loan_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
