0314d3ae4f28249eaf8ba54de6f92ec9b4d143cf
yurijatki <82046569+yurijatki@users.noreply.github.com>
2025-11-14 14:28:26 -0300

    listando datas e ordenando

--- src/main/java/com/test/loan_calculator/service/LoanCalculatorService.java
+++ src/main/java/com/test/loan_calculator/service/LoanCalculatorService.java
@@ -1,7 +1,9 @@
 package com.test.loan_calculator.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,11 @@ import lombok.val;
 @Service
 public class LoanCalculatorService {
 
+    public LoanCalculatorService(LoanValidator validator) {
+        this.validator = validator;
+    }
+
+
     @Autowired
     private LoanValidator validator;
 
@@ -22,6 +29,42 @@ public class LoanCalculatorService {
         
         validator.validate(loanRequest);
 
+        List<LoanResponseDTO> loans = new ArrayList<>();
+        
+        TreeSet<LocalDate> paymentDates = generateAllPaymentDates(loanRequest);
+
+        System.out.println("Datas de pagamento geradas: "+paymentDates.toString());
+        //ordenando datas
+        paymentDates.stream().sorted();
+
+        System.out.println("Datas de pagamento ordenadas: "+paymentDates.toString());
+
         return new ArrayList<>();
+    }
+
+    private TreeSet<LocalDate> generateAllPaymentDates(LoanRequestDTO loan) {
+        TreeSet<LocalDate> dates = new TreeSet<>();
+
+        dates.add(loan.getStartDate());
+        dates.add(loan.getFirstPaymentDate());
+
+        LocalDate currentDate = loan.getFirstPaymentDate();
+        if(loan.getStartDate().getMonth() != loan.getFirstPaymentDate().getMonth() &&
+           loan.getStartDate().getYear() != loan.getFirstPaymentDate().getYear()) {
+            // grava ultimo dia do mes da data inicial se for diferente do mes do primeiro pagamento
+            dates.add(loan.getStartDate().withDayOfMonth(loan.getStartDate().lengthOfMonth()));
+        }
+        while (!currentDate.isAfter(loan.getEndDate())) {
+            LocalDate lastDateOfMonth = currentDate.withDayOfMonth(currentDate.lengthOfMonth());
+            
+            currentDate = currentDate.plusMonths(1);
+            if(currentDate.isAfter(loan.getEndDate())) break;
+
+            dates.add(currentDate);
+            if(!lastDateOfMonth.isAfter(loan.getEndDate())) dates.add(lastDateOfMonth);
+
+        }
+
+        return dates;
     }
 }
